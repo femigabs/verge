4f9ce0d9808812a6d3de67a95b7b831d11167464
Oluwafemi Babayemi <femi.gabs@gmail.com>
2020-04-27 17:25:52 +0100

    update Authorization code

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const db = require("./database");
+const user = require("./Routes/userRoute"); 
+const admin = require("./Routes/adminRoute");
+const parcel = require("./Routes/parcelRoute")
+
+
+let app = express();
+let port = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(port, () => {
+console.log("Application Listening on Port 4000")
+});
+
+app.get('/', (req, res) => {
+return res.status(200).json({
+message: "welcome to my Parcel Delivery Service"
+});
+});
+app.use("/api/v1", user);
+app.use("/api/v1", admin);
+app.use("/api/v1", parcel);
+
+module.exports = app;
